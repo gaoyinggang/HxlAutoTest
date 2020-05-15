ec87af274f88e7a0ef58de4e5149306046c5a103
huangxiaole <xiaole.huang-p0059@aqara.com>
2020-05-15 15:03:46 +0800

    提交

--- MavenSeleniumProject/src/test/java/pageobject/LoginPage.java
+++ MavenSeleniumProject/src/test/java/pageobject/LoginPage.java
@@ -2,6 +2,7 @@ package pageobject;
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 
 import utils.BaseTest;
@@ -9,12 +10,16 @@ import utils.UseBrowser;
 
 public class LoginPage extends BaseTest{
 	private static final Logger LOGGER = Logger.getLogger(LoginPage.class);
+//	JavascriptExecutor js = (JavascriptExecutor) driver; 
 		
 	public void login(String username, String password) throws Exception  {
 		
-		
-		driver.findElement(By.id("lbNormal")).click();
-		Thread.sleep(2000);	
+		//判断邮箱密码登录
+		WebElement element=driver.findElement(By.id("appLoginTab"));
+		if(element.isDisplayed()) {
+			driver.findElement(By.id("lbNormal")).click();
+			Thread.sleep(2000);
+		}			
 //		进入iframe
 		driver.switchTo().frame(driver.findElement(By.xpath("//*[@id='loginDiv']/descendant::iframe[1]")));
 //		输入用户名密码
